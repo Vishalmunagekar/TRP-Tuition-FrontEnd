ef2e486535a045b5f1369fdb642effbdf3f736f1
munagvis <vishal.munagekar@tieto.com>
2021-10-02 11:23:40 +0530

    initial commit

---
+++ apps/erp-tuition-ng/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'erp-tuition-frontend-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'erp-tuition-ng';
+}
