5121bd9890aa4a8146c203e56d72e7c919bc3816
munagvis <vishal.munagekar@tieto.com>
2021-10-09 23:17:47 +0530

    icon module created and installed font-awesome icons

--- apps/erp-tuition-ng/src/app/app.module.ts
+++ apps/erp-tuition-ng/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { IconModule } from './modules/icon/icon.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, NgbModule],
+  imports: [BrowserModule, NgbModule, FontAwesomeModule, IconModule],
   providers: [],
   bootstrap: [AppComponent],
 })
